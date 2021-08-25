b538b7fe8a8cd5a27154768c0e4132ab8294302d
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-08-25 15:50:59 -0300

    Update login page

--- bookmark-landing-page/js/index.js
+++ bookmark-landing-page/js/index.js
@@ -1,7 +1,9 @@
 import menuMobile from './scripts/menuMobile.js';
 import handleEl from './scripts/handleEl.js';
+import smoothScroll from './scripts/smoothScroll.js';
 
 window.onload = () => {
     menuMobile();
     handleEl();
+    smoothScroll();
 };
